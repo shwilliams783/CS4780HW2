d78a5065484f575b712e12aad6b564e114921ff6
shwilliams783 <shwilliams783@gmail.com>
2017-10-03 15:38:37 -0500

    Shared Memory Implementation
    The program now allocates, uses, and deallocates shared memory with none left over.

--- master.c
+++ master.c
@@ -7,14 +7,17 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
+#define BUFFER 1000
+
 int main (int argc, char *argv[]) {  
 int o;
-char c[1000];
+char c[BUFFER];
 int shmid;
-key_t key;
+key_t key = 4780;
 char *shm, *s;
 FILE *fp;
 
+/* Options */
 while ((o = getopt (argc, argv, "h")) != -1)
 	switch (o)
     {
@@ -27,20 +30,38 @@ while ((o = getopt (argc, argv, "h")) != -1)
 			break;
     }
 
+/* Open the Input file */
 fp = fopen("./Input.txt", "r");
-if(fp == NULL){
+if(fp == NULL)
+{
 	perror("fopen");
 }
 
-printf("Data from the file:\n%s", c);
-while (fgets(c, sizeof(c), fp))
+/* Create shared memory segment */
+if ((shmid = shmget(key, BUFFER, IPC_CREAT | 0666)) < 0) 
 {
-	printf("%s", c);
+	perror("shmget");
+    exit(1);
+}
+
+/* Point shm to shared memory */
+if ((shm = shmat(shmid, NULL, 0)) == (char *) -1)
+{
+	perror("shmat");
+    exit(1);
+}
+
+/* Read the file */
+while (fgets(shm, BUFFER, fp))
+{
+	printf("%s", shm);
 }
 printf("File printing complete!\n");
 
-
-
+/* Release shared memory */
+shm = NULL;
+shmdt(shm);
+shmctl(shmid, IPC_RMID, NULL);
 fclose(fp);
 	
 return 0;
