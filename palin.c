d0a779df4c59af0c1e8a227c2c59374e2de93b56
shwilliams783 <shwilliams783@gmail.com>
2017-10-05 16:54:35 -0500

    Command Line Arguments
    palin.c now takes ID, index, and key as arguments
    palin.c now accesses shared memory allocated by master (no, really).

--- palin.c
+++ palin.c
@@ -11,15 +11,42 @@
 #define MAXPROC 95
 
 int main (int argc, char *argv[]) {
+int i;
 int errno;
-int index = 0;
+int id = atoi(argv[1]);
+int index = atoi(argv[2]);
+int key = atoi(argv[3]);
 pid_t pid = getpid();
 int shmidArray;
 key_t keyArray = 8675;
 char *shmArrayPtr[BUFFER];
 char shmArray[MAXPROC][BUFFER];
 
+/* Point shmArray to shared memory */
+*shmArrayPtr = shmat(key, NULL, 0);
+if ((void *)shmArray == (void *)-1)
+{
+    perror("PALIN: shmat: key");
+    exit(1);
+}
+
 printf("\nPalin.c Executed!\n");
+for(i = 0; i < 5; i++)
+{
+	printf("%d\t%d\t%s\n", pid, index, (*shmArrayPtr + (index*BUFFER)));
+	index++;
+}
+
+errno = shmdt(*shmArrayPtr);
+if(errno == -1)
+{
+	perror("PALIN: shmdt: shmArrayPtr");
+}
+/*errno = shmctl(key, IPC_RMID, NULL);
+if(errno == -1)
+{
+	perror("PALIN: shmctl: key");
+}*/
 
 return 0;
 }
