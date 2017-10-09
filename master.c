a1d84460baa79f5a5fe894c66309234ef35a3ea8
shwilliams783 <shwilliams783@gmail.com>
2017-10-09 16:46:31 -0500

    Signal Handling
    Added <signal.h>
    added signal handlers for SIGINT
    added kill() calls for each child in the event of a timeout after 60 seconds

--- master.c
+++ master.c
@@ -3,21 +3,72 @@
 #include <unistd.h>
 #include <string.h>
 #include <getopt.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
 #define BUFFER 1000
-#define MAXPROC 95
+#define MAXPROC 96
 
+int errno;
+enum state { idle, want_in, in_cs };
+int shmidArray;
+int shmidTurn;
+int shmidFlag;
+char *shmArrayPtr;
+int *turn;
+enum state *flag;
 
+void sigIntHandler(int signum)
+{
+	perror("Caught SIGINT! Killing all child processes.");
+	
+	errno = shmdt(shmArrayPtr);
+	if(errno == -1)
+	{
+		perror("MASTER: shmdt: shmArray");
+	}
+	
+	errno = shmctl(shmidArray, IPC_RMID, NULL);
+	if(errno == -1)
+	{
+		perror("MASTER: shmctl: shmidArray");
+	}
+
+	errno = shmdt(turn);
+	if(errno == -1)
+	{
+		perror("MASTER: shmdt: turn");
+	}
+	
+	errno = shmctl(shmidTurn, IPC_RMID, NULL);
+	if(errno == -1)
+	{
+		perror("MASTER: shmctl: shmidTurn");
+	}
+
+	errno = shmdt(flag);
+	if(errno == -1)
+	{
+		perror("MASTER: shmdt: flag");
+	}
+	
+	errno = shmctl(shmidFlag, IPC_RMID, NULL);
+	if(errno == -1)
+	{
+		perror("MASTER: shmctl: shmidFlag");
+	}
+	
+	exit(signum);
+}
 
 
 
 int main (int argc, char *argv[]) {
-enum state { idle, want_in, in_cs };
+
 int o;
-int errno;
+int i;
 int returnStatus;
 int index = 0;
 pid_t pid[20] = {getpid()};
@@ -27,16 +78,11 @@ char indexArg[33];
 char arrayArg[33];
 char turnArg[33];
 char flagArg[33];
-int shmidArray;
-int shmidTurn;
-int shmidFlag;
 key_t keyArray = 8675;
 key_t keyTurn = 1138;
 key_t keyFlag = 1123;
-char *shmArrayPtr;
-int *turn;
-enum state *flag;/*Flag corresponding to each process in shared memory */
 FILE *fp;
+signal(SIGINT, sigIntHandler);
 
 
 /* Options */
@@ -122,6 +168,7 @@ for(index = 0; index < 95; index++)
 	fgets((shmArrayPtr + (index*BUFFER)), BUFFER, fp);
 	/* printf("%d, %s", index, (shmArrayPtr + (index * BUFFER))); */
 }
+fclose(fp);
 
 /* Fork off child processes */
 for(index = 0; index < 20; index++)
@@ -149,46 +196,53 @@ for(index = 0; index < 20; index++)
 	}
 }
 
-/* Release shared memory */
-/*if(pid != 0)
+/* Signal handler for killing child processes */
+for(i = 0; i < 60; i++)
 {
-	errno = shmdt(*shmArrayPtr);
-	if(errno == -1)
-	{
-		perror("MASTER: shmdt: shmArray");
-	}
-	errno = shmctl(shmidArray, IPC_RMID, NULL);
-	if(errno == -1)
-	{
-		perror("MASTER: shmctl: shmidArray");
-	}
-	
-	errno = shmdt(*turn);
-	if(errno == -1)
-	{
-		perror("MASTER: shmdt: turn");
-	}
-	errno = shmctl(shmidTurn, IPC_RMID, NULL);
-	if(errno == -1)
-	{
-		perror("MASTER: shmctl: shmidTurn");
-	}
-	
-	errno = shmdt(*flag);
-	if(errno == -1)
-	{
-		perror("MASTER: shmdt: flag");
-	}
-	errno = shmctl(shmidFlag, IPC_RMID, NULL);
-	if(errno == -1)
-	{
-		perror("MASTER: shmctl: shmidFlag");
-	}
-	
-	fclose(fp);
-	*shmArrayPtr = NULL;
-	*turn = NULL;
-	*flag = NULL;
-}*/
+	sleep(1);
+}
+
+perror("Time is up!");
+
+/* Release shared memory */
+errno = shmdt(shmArrayPtr);
+if(errno == -1)
+{
+	perror("MASTER: shmdt: shmArray");
+}
+errno = shmctl(shmidArray, IPC_RMID, NULL);
+if(errno == -1)
+{
+	perror("MASTER: shmctl: shmidArray");
+}
+
+errno = shmdt(turn);
+if(errno == -1)
+{
+	perror("MASTER: shmdt: turn");
+}
+errno = shmctl(shmidTurn, IPC_RMID, NULL);
+if(errno == -1)
+{
+	perror("MASTER: shmctl: shmidTurn");
+}
+
+errno = shmdt(flag);
+if(errno == -1)
+{
+	perror("MASTER: shmdt: flag");
+}
+errno = shmctl(shmidFlag, IPC_RMID, NULL);
+if(errno == -1)
+{
+	perror("MASTER: shmctl: shmidFlag");
+}
+
+for(i = 1; i < 20; i++)
+{
+	/* printf("Killing process #%d\n", pid[i]); */
+	kill(pid[i], SIGINT);
+}
+
 return 0;
 }
